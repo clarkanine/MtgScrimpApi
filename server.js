c23d50118166b71eb698099fcffd6327cf2e761e
Aaron Clark <aclark56@eagles.ewu.edu>
2018-06-09 14:39:42 -0600

    access control allow origin added

--- server.js
+++ server.js
@@ -36,4 +36,19 @@ console.log('todo list RESTful API server started on: ' + port);
 
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'https://stark-headland-48165.herokuapp.com/');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
   });
